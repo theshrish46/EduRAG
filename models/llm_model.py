bf067a7fe1153b9d6c2cf5a28396b18accb03d97
Shrish Kerur <theshrish46@gmail.com>
2025-12-08 23:12:31 +0530

    fixing env error

--- models/llm_model.py
+++ models/llm_model.py
@@ -1,12 +1,15 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
+import os
 
 load_dotenv()
+HF_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 llm = HuggingFaceEndpoint(
     repo_id= "Qwen/Qwen3-Coder-30B-A3B-Instruct",
-    task= "text-generation"
+    task= "text-generation",
+    huggingfacehub_api_token=HF_TOKEN
 )
 
 def get_llm_model():
